6ea1bc07a6282867a6e387886e327ec6b9df9bdd
Eduardo Figueiredo <figueiredo92002@gmail.com>
2024-06-06 18:00:37 +0100

    xico da tina

--- WalletBud/src/main/java/WB/walletbud/SystemInterface.java
+++ WalletBud/src/main/java/WB/walletbud/SystemInterface.java
@@ -13,17 +13,30 @@ public class SystemInterface {
     }
 
     public static void main(String[] args) {
-        SystemInterface si = new SystemInterface();
+        //SystemInterface si = new SystemInterface();
 
         try {
-            si.createUser("Paco Nassa", "1234", "pn@gmail.com");
+//            createUser("Rois", "1234", "r@gmail.com");
+            verifyUser("r@gmail.com", "1234");
         } catch (PersistentException e) {
             System.out.println("ERROOOOOOOOO");
         }
         System.exit(0);
     }
 
-    public static void createUser(String name, String password, String email) throws PersistentException {
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public static boolean createUser(String name, String password, String email) throws PersistentException {
         PersistentTransaction t = AASICPersistentManager.instance().getSession().beginTransaction();
         try {
 
@@ -34,17 +47,46 @@ public class SystemInterface {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] encodedhash = digest.digest(
                     password.getBytes(StandardCharsets.UTF_8));
+            String hashedPassword = bytesToHex(encodedhash);
 
-            user.setPassword(encodedhash.toString());
+            user.setPassword(hashedPassword);
 
             UserDAO.save(user);
 
             t.commit();
             System.out.println("User inserido!");
+            return true;
         } catch (Exception e) {
             t.rollback();
             System.out.println("User não inserido!");
+            return false;
         }
     }
+
+    public static int verifyUser(String email, String password) throws PersistentException {
+
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedhash = digest.digest(
+                    password.getBytes(StandardCharsets.UTF_8));
+            String hashedPassword = bytesToHex(encodedhash);
+
+            String condition = "email = '" + email + "' AND password = '" + hashedPassword + "'";
+
+            User[] users = UserDAO.listUserByQuery(condition, null);
+
+            if (users.length == 0) {
+                return -1;
+            } else {
+                return 0;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -2;
+        }
+
+    }
+
 
 }

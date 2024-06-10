dc9b7c8cb3aaff4b410c288fd3c83d1965ab8b7f
Eduardo Figueiredo <figueiredo92002@gmail.com>
2024-06-11 00:03:28 +0100

    Changes by GSenra

--- WalletBud/src/main/java/com/walletbud/Utilizador.java
+++ WalletBud/src/main/java/com/walletbud/Utilizador.java
@@ -4,10 +4,13 @@ import jakarta.ejb.EJB;
 import beans.stateless.GerirUtilizador;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.io.StringReader;
 
 @Path("/user")
 public class Utilizador {
@@ -71,6 +74,57 @@ public class Utilizador {
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @POST
+    @Path("/set")
+    @Secured
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response editUser(String jsonString, @HeaderParam(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+        String token = authorizationHeader.substring("Bearer ".length()).trim();
+        String email_user = JWTUtil.getEmailFromToken(token);
+
+
+        JsonReader reader = Json.createReader(new StringReader(jsonString));
+        JsonObject jsonObject = reader.readObject();
+        reader.close();
+
+        System.out.println("Received JSON: " + jsonObject.toString());
+
+        String username, password, idioma;
+
+        try{username = jsonObject.getString("username");}catch(java.lang.NullPointerException en) {username=null;}
+        try{password = jsonObject.getString("password");}catch(java.lang.NullPointerException en) {password=null;}
+        try{idioma = jsonObject.getString("idioma");}catch(java.lang.NullPointerException en) {idioma=null;}
+
+
+        try {
+            if(gerirUtilizador.editUser(username, password, email_user, idioma) ){
+
+                JsonObject jsonResponse = Json.createObjectBuilder()
+                        .add("message", "User edited successfully")
+                        .build();
+
+                return Response.status(Response.Status.CREATED)
+                        .entity(jsonResponse.toString())
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }else{
+                JsonObject jsonResponse = Json.createObjectBuilder()
+                        .add("message", "Algo de errado nao esta certo!")
+                        .build();
+
+                return Response.status(Response.Status.BAD_REQUEST)
+                        .entity(jsonResponse.toString())
+                        .type(MediaType.APPLICATION_JSON)
+                        .build();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error while creating user: " + e.getMessage());
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }

35f1a30ed4b25c602ad72959281e3f03a977f942
Eduardo Figueiredo <figueiredo92002@gmail.com>
2024-06-19 21:23:37 +0100

    [fix] agenda a funcionar

--- WalletBud_FrontEnd/src/views/Schedule.vue
+++ WalletBud_FrontEnd/src/views/Schedule.vue
@@ -1,55 +1,43 @@
 <template>
-    <div class="container">
+    <div class="container d-flex flex-column" style="height: 100vh;">
         <div class="row justify-content-center">
             <div class="timeline-header mb-4 d-flex justify-content-between align-items-center">
-                <h4 class="mb-0">{{ $t('Agenda')}}</h4>
+                <h4 class="mb-0">{{ $t('Agenda') }}</h4>
                 <div class="d-flex align-items-center">
-
-                    <!-- time period-->
-                        <MaterialDropdown
-                            :on-click="loadTimeline"
-                            :options="months"
-                            v-model:selected="selectedMonth"
-                            class="me-2"
-                            style="width: 110px;"
-                        />
-
-                        <MaterialDropdown
-                            :on-click="loadTimeline"
-                            :options="years"
-                            v-model:selected="selectedYear"
-                            class="me-2"
-                            style="width: 80px;"
-                        />
-
+                    <!-- time period -->
+                    <MaterialDropdown :options="months" v-model:selected="selectedMonth"
+                    class="me-2" style="width: 110px;" />
+                    <MaterialDropdown :options="years" v-model:selected="selectedYear"
+                        class="me-2" style="width: 80px;" />
                 </div>
             </div>
+        </div>
 
-            <div class="scroll-container">
-                <div v-if="store.timeline.length > 0">
-                    <timeline-list class="cuter">
-                        <timeline-item v-for="(p, index) in store.timeline" :key="index"
-                            :color="p.tipo === 'receita' ? 'success' : 'danger'"
-                            :icon="{ component: p.tipo === 'receita' ? 'notifications' : 'shopping_cart', class: 'text-white text-sm' }"
-                            :title="p.name"
-                            :value="p.tipo === 'receita' ? (p.value + ' ' + user.moeda) : ('-' + p.value + ' ' + user.moeda)"
-                            :date-time="p.date" :description="p.descricao"
-                            :date-color="p.tipo === 'receita' ? 'success' : 'danger'" />
-                    </timeline-list>
-                </div>
-                <div v-else class="d-flex justify-content-center align-items-center" style="height: 70vh;">
-                    <h5 class="text-secondary">{{ $t('Sem eventos agendados')}}</h5>
-                </div>
+        <div class="scroll-container flex-grow-1">
+            <div v-if="store.timeline.length > 0">
+                <timeline-list class="cuter">
+                    <timeline-item v-for="(p, index) in store.timeline" :key="index"
+                        :color="p.tipo === 'receita' ? 'success' : 'danger'"
+                        :icon="{ component: p.tipo === 'receita' ? 'notifications' : 'shopping_cart', class: 'text-white text-sm' }"
+                        :title="p.name"
+                        :value="p.tipo === 'receita' ? (p.value + ' ' + user.moeda) : ('-' + p.value + ' ' + user.moeda)"
+                        :date-time="p.date" :description="p.descricao"
+                        :date-color="p.tipo === 'receita' ? 'success' : 'danger'" />
+                </timeline-list>
+            </div>
+            <div v-else class="d-flex justify-content-center align-items-center" style="height: 70vh;">
+                <h5 class="text-secondary">{{ $t('Sem eventos agendados') }}</h5>
             </div>
         </div>
     </div>
 </template>
 
+
 <script>
 import TimelineList from "@/examples/Cards/TimelineList.vue";
 import TimelineItem from "@/examples/Cards/TimelineItem.vue";
 import MaterialDropdown from "@/components/MaterialDropdown.vue";
-import { ref, computed, onMounted, watch } from 'vue';
+import { ref, onMounted, watch } from 'vue';
 import { fixaStore } from "@/store/fixaStore";
 import { userStore } from "@/store/userStore";
 import { useRouter } from 'vue-router';
@@ -62,55 +50,51 @@ export default {
         const { t, locale } = useI18n();
 
         const snackbar = ref(false);
-        const popup = ref(false);
-        const popupReject = ref(false);
-        const popupAccept = ref(false);
-        const popupDetails = ref(false);
-        const popupReject2 = ref(false);
-        const popupAccept2 = ref(false);
         const store = fixaStore();
         const user = userStore();
-        const selectedPendenteId = ref(null);
-        const transa = ref(null);
         const router = useRouter();
-        
+
         // Array de meses inicial
         let months = [
-        t('Janeiro'),
-        t('Fevereiro'),
-        t('Março'),
-        t('Abril'),
-        t('Maio'),
-        t('Junho'),
-        t('Julho'),
-        t('Agosto'),
-        t('Setembro'),
-        t('Outubro'),
-        t('Novembro'),
-        t('Dezembro')
+            t('Janeiro'),
+            t('Fevereiro'),
+            t('Março'),
+            t('Abril'),
+            t('Maio'),
+            t('Junho'),
+            t('Julho'),
+            t('Agosto'),
+            t('Setembro'),
+            t('Outubro'),
+            t('Novembro'),
+            t('Dezembro')
         ];
 
         // Observa mudanças no locale
         watch(locale, () => {
-        // Atualiza cada elemento do array individualmente
-        months.forEach((_, index) => {
-            months[index] = t(months[index]);
-        });
+            // Atualiza cada elemento do array individualmente
+            months.forEach((_, index) => {
+                months[index] = t(months[index]);
+            });
 
-        // Opcional: Atualiza o mês selecionado
-        selectedMonth.value = months[new Date().getMonth()];
+            // Opcional: Atualiza o mês selecionado
+            selectedMonth.value = months[new Date().getMonth()];
         });
 
         // Variável reativa para o mês selecionado
         const selectedMonth = ref(months[new Date().getMonth()]);
-        
-        
+
+
         const years = Array.from({ length: 10 }, (_, i) => new Date().getFullYear() - 5 + i);
         const selectedYear = ref(new Date().getFullYear());
 
         const loadTimeline = async () => {
             try {
-                await store.loadTimeline(selectedYear.value, selectedMonth.value + 1);
+                let month = months.indexOf(selectedMonth.value);
+                console.log("Carregando timeline...");
+                console.log("Ano: " + selectedYear.value);
+                console.log("Mês: " + month);
+                await store.loadTimeline(selectedYear.value, month + 1);
                 console.log("Timeline carregada:", store.timeline);
             } catch (err) {
                 if (err.message.includes('token')) {
@@ -126,106 +110,26 @@ export default {
             }
         };
 
-        const getRepetitionText = (repeticao) => {
-            if (repeticao === undefined) {
-                return "Unica";
-            }
-            switch (repeticao) {
-                case 1:
-                    return "Diariamente";
-                case 2:
-                    return "Semanalmente";
-                case 3:
-                    return "Mensalmente";
-                case 4:
-                    return "Anualmente";
-                default:
-                    return "Unica";
-            }
-        };
-
-        const formattedDate = (timestamp) => {
-            const date = new Date(timestamp.replace(' ', 'T'));
-            const day = String(date.getDate()).padStart(2, '0');
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const year = date.getFullYear();
-            const hours = String(date.getHours()).padStart(2, '0');
-            const minutes = String(date.getMinutes()).padStart(2, '0');
-
-            return `${day}-${month}-${year} ${hours}:${minutes}`;
-        }
-
-        const openDetailsPopup = (id) => {
-            selectedPendenteId.value = id;
-            popupDetails.value = true;
-        };
-
-        const selectedPendente = computed(() => {
-            return store.pendentes.find(p => p.id === selectedPendenteId.value) || {};
+        // Watchers para observar mudanças nos dropdowns
+        watch(selectedMonth, () => {
+            console.log('Selected month changed:', selectedMonth.value);
+            loadTimeline();
         });
 
-        const openPopup = async (id) => {
-            popup.value = true;
-            const t = store.proximosPagamentos.find(p => p.id === id);
-
-            const url = "http://localhost:8000/WalletBud-1.0-SNAPSHOT/api/fixa/" + t.tipo + "/get/" + t.id;
-            const token = localStorage.getItem('token');
-
-            const request = {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    'Authorization': "Bearer " + token
-                },
-            };
-
-            try {
-                const response = await fetch(url, request);
-
-                if (!response.ok) {
-                    const errorData = await response.json();
-                    throw new Error(errorData.message);
-                }
-
-                const data = await response.json();
-                console.log(data);
-
-                // Mapear os dados recebidos para o formato de categorias desejado
-                transa.value = data;
-
-            } catch (error) {
-                console.error('Erro ao carregar pendentes:', error.message);
-                throw error;
-            }
-        };
-
-        const closeSnackbar = () => {
-            snackbar.value = false;
-        };
+        watch(selectedYear, () => {
+            console.log('Selected year changed (watch):', selectedYear.value);
+            loadTimeline();
+        });
 
         onMounted(() => {
             loadTimeline();
         });
 
         return {
-            formattedDate,
             loadTimeline,
             snackbar,
-            closeSnackbar,
-            popup,
-            popupReject,
-            popupAccept,
-            popupDetails,
-            popupReject2,
-            popupAccept2,
-            getRepetitionText,
             store,
             user,
-            transa,
-            selectedPendenteId,
-            openDetailsPopup,
-            selectedPendente,
-            openPopup,
             selectedMonth,
             selectedYear,
             months,
@@ -235,8 +139,6 @@ export default {
     },
 };
 </script>
-
-
 
 <style scoped>
 .scroll-container {
